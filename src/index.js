35361aba30238892df9f019f7b743989e0b0763d
rabah babaci <4rabah@gmail.com>
2020-04-09 16:41:13 -0700

    in index.js create the redux store

--- src/index.js
+++ src/index.js
@@ -5,5 +5,12 @@ import App from './App';
 import 'bulma/css/bulma.css';
 import './styles.scss';
 
+import { createStore } from 'redux';
+import { Provider } from 'react-redux';
+import reducer from './reducers/index';
+
+const store = createStore(reducer);
+console.log(store);
+
 const rootElement = document.getElementById('root');
 ReactDOM.render(<App />, rootElement);
